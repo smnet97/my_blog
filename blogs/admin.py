a0874b7c2003c0d10564ae72b82ce49d980c852b
shranet <shranet@gmail.com>
2022-06-27 11:03:51 +0500

    ...

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import BlogModel, CategoryModel
 
-# Register your models here.
+
+@admin.register(CategoryModel)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+
+
+@admin.register(BlogModel)
+class BlogModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    list_display_links = ['title', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title', 'body']
--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
     path('blogs/', include('blogs.urls')),
     path('works/', include('works.urls')),
